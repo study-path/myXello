65c1317c732515234764d2925dc94cbcd6ee7298
LiliyaIngul <liliyaingul@gmail.com>
2021-03-15 23:43:03 -0400

    initial BE

---
+++ BE/MyXello.Api/Controllers/LessonsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyXello.Api.Controllers.ViewModels;
+
+namespace MyXello.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LessonsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<LessonViewModel> Get()
+        {
+            return new[] {
+                new LessonViewModel {
+                    Id=1,
+                    Name="Math"
+                },
+                new LessonViewModel{
+                    Id=2,
+                    Name="Gym"
+                }
+            };            
+        }
+    }
+}
---
+++ BE/MyXello.Api/Controllers/ViewModels/LessonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyXello.Api.Controllers.ViewModels
+{
+    public class LessonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
